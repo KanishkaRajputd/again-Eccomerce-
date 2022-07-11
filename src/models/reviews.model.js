3fb3022b756cb3868c5f4765eb53862331992954
Kanishka Rajput <kanishka6393rajput@gmail.com>
2022-07-11 09:55:16 +0530

    submited

---
+++ src/models/reviews.model.js
@@ -0,0 +1,22 @@
+const mongoose=require("mongoose")
+const reviewSchema=new mongoose.Schema({
+
+userId:[{
+    ref:"myusers",
+    type:mongoose.Schema.Types.ObjectId,
+    required:true
+}]
+,
+description :{type:String ,required:true},
+rating:  {type:Number,required:true},
+productId:{ref:"products",
+type:mongoose.Schema.Types.ObjectId,
+required:true}
+}
+,{
+    timestamps:true,
+    versionKey:false
+})
+
+
+module.exports=mongoose.model("reviews",reviewSchema);
